ac4d01cddb040a249b4968305661df13b5f38b05
Nasir Digital Skills <uk278109@gmail.com>
2026-09-12 14:01:22 +0500

    Update appwrite-config.js

--- appwrite-config.js
+++ appwrite-config.js
@@ -1,1 +1,6 @@
-export const APPWRITE_ENDPOINT="https://cloud.appwrite.io/v1";export const APPWRITE_PROJECT_ID="PASTE_PROJECT_ID";export const DATABASE_ID="world";export const MOMENTS_COLLECTION_ID="moments";export const USERS_COLLECTION_ID="users";
+export const APPWRITE_ENDPOINT = "https://nyc.cloud.appwrite.io/v1";
+export const APPWRITE_PROJECT_ID = "6aa5127e001ea31b0f77";
+
+export const DATABASE_ID = "world";
+export const MOMENTS_COLLECTION_ID = "moments";
+export const USERS_COLLECTION_ID = "users";
