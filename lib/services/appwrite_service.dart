3f07b46c53e2c99cc7a515682c2bdd3200a82076
Nasir Digital Skills <uk278109@gmail.com>
2026-09-15 10:24:57 +0500

    Create appwrite_service.dart

---
+++ lib/services/appwrite_service.dart
@@ -0,0 +1,13 @@
+import 'package:appwrite/appwrite.dart';
+import '../config/app_config.dart';
+
+class AppwriteService {
+  static final Client client = Client()
+    ..setEndpoint(AppConfig.appwriteEndpoint)
+    ..setProject(AppConfig.appwriteProjectId);
+
+  static final Account account = Account(client);
+  static final Databases databases = Databases(client);
+  static final Realtime realtime = Realtime(client);
+  static final Storage storage = Storage(client);
+}
