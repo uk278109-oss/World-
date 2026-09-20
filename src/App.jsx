6366f2c6e7ed8cfcee26c2feca4701361ea0dbd0
Nasir Digital Skills <uk278109@gmail.com>
2026-09-20 15:40:56 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import OnboardingFlow from "./flows/onboarding/OnboardingFlow";
 import "./flows/onboarding/onboarding.css";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<OnboardingFlow />} />
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<OnboardingFlow />} />
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </Routes>
   );
 }
