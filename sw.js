d32698068e521da42782d1e8a8d1a7278647b93d
Nasir Digital Skills <uk278109@gmail.com>
2026-09-20 13:50:20 +0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,18 @@
+const CACHE = 'world-shell-v1';
+const ASSETS = ['/', '/index.html', '/manifest.webmanifest'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request))
+  );
+});
